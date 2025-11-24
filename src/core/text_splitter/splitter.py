3e448003624cabb552ba062766e846e0798cd724
Dybal-Ilia <dybal.i156@gmail.com>
2025-11-24 09:36:19 +0100

    refactor: update Splitter class to enhance chunking logic and improve logging

--- src/core/text_splitter/splitter.py
+++ src/core/text_splitter/splitter.py
@@ -1,11 +1,16 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from src.core.utils import logger
 class Splitter:
-    def __init__(self, max_chunk_size: int = 1000, chunk_overlap: int = 200):
-        self.text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=max_chunk_size,
-            chunk_overlap=chunk_overlap
-        )
+    def __init__(self, max_chunk_size: int = 2000, chunk_overlap: int = 300):
+        self.splitter = RecursiveCharacterTextSplitter(
+        chunk_size=max_chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", ".", "!", "?", " ", ""],
+    )
 
-    def split(self, documnets):
-        return self.text_splitter.split_documents(documents=documnets)
+    def split(self, documents):
+        chunks = []
+        for doc in documents:
+            chunks.extend(self.splitter.split_text(doc.text))
+        logger.info(f"Total {len(chunks)} created")
+        return chunks
