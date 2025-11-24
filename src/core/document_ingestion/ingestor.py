04c17b53c5f6e99b6fd87c8ac5e0dc38dec41498
Dybal-Ilia <dybal.i156@gmail.com>
2025-11-24 09:36:07 +0100

    feat: implement LlamaParse for document ingestion and remove legacy loaders

--- src/core/document_ingestion/ingestor.py
+++ src/core/document_ingestion/ingestor.py
@@ -1,28 +1,32 @@
-from langchain_community.document_loaders import(
-    TextLoader,
-    PyMuPDFLoader,
-    Docx2txtLoader,
-    UnstructuredHTMLLoader,
-    UnstructuredEPubLoader,
-)
+from llama_cloud_services import LlamaParse
+from src.core.utils import logger
+from dotenv import load_dotenv
+import os
 
-SPLITTERS = {
-    ".xt": TextLoader,
-    "md": TextLoader,
-    "docx": Docx2txtLoader,
-    "pdf": PyMuPDFLoader,
-    "html": UnstructuredHTMLLoader,
-    "epub": UnstructuredEPubLoader
-}
+load_dotenv()
+
+LLAMA_CLOUD_API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
 
 class Ingestor:
-    def load(self, path: str):
-        split = path.split(".")[-1]
-        try:
-            loader = SPLITTERS[split](path)
-        except Exception as e:
-            raise e
-        
-        return loader.load()
-            
+    def __init__(self):
+        self.parser = LlamaParse(
+            api_key=LLAMA_CLOUD_API_KEY,
+            verbose=True,
+            parse_mode="parse_page_with_llm"
+        )
     
+    
+    async def ingest(self, path:str):
+        try:
+            documents = await self.parser.aparse(path)
+            markdwon_documents = documents.get_markdown_documents()
+            logger.info("Document parsed successfully")
+            return markdwon_documents
+        except FileNotFoundError:
+            logger.error("Seems like {path} does not exist")
+            return []
+        except Exception as e:
+            logger.error(f"An error occured while parsing document: {str(e)}")
+            return []
+
+
